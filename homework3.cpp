b07b7621e2375b64668c0905a939c2ff3085e905
Michael <mi.kestner@outlook.com>
2025-09-18 10:46:02 -0400

    Complete. Thanks Andrew.

--- homework3.cpp
+++ homework3.cpp
@@ -10,6 +10,7 @@
 #include <string>
 #include <stdexcept>
 #include <sstream>
+#include <iomanip>
 #include "Movie.h"
 
 // A shortcut to a map of movies. The key for this map 
@@ -54,55 +55,55 @@ void findMovie(const MovieMap& db, const int id) {
   } else {
     std::cout << "Movie with ID " << id << 
     " not found in database." << std::endl;
-    // std::cout << "ID " << id << " not found!" << std::endl;
   }
 }
 
 void searchMovie(const MovieMap& db, std::string& value) {
   // Count how many movies the search found.
   int count = 0;
-  for (const auto& movie : db) {
-    std::string entry = to_string(movie.second);
+  for (const auto& ref : db) {
+    const Movie movie = ref.second;
+    std::string entry = to_string(movie);
     if (entry.find(value) != std::string::npos) {
       std::cout << entry << std::endl;
       count++;
     }
   }
   // Print the counter.
-  std::cout << "Found " << count << " matche(s)" << std::endl;
+  std::cout << "Found " << count << " matche(s)." << std::endl;
 }
 
 int main(int argc, char** argv) {
   // Load the movie database from a given file.
   MovieMap db = load("./movies_db.txt");
 
-  std::string command, line;
+  std::string command;
 
-  while (true) {
-    // Assign values to variables from the user input.
-    std::cout << "Enter a command:" << std::endl;
-    std::getline(std::cin, line);
-    std::istringstream iss(line);
-    iss >> command;
-
-    // Quit the program if the user wants to exit.
-    if (command == "exit") { break; }
-
-    std::string value;
-    std::getline(iss, value);
-
-    // Ignore any spaces in front of commands
-    size_t first = value.find_first_not_of(' ');
-    if (first != std::string::npos) {
-      value = value.substr(first);
-    } else { value = ""; }
-
-    // Call helper methods to find movie IDs or search.
-    if (command == "find") {
-      findMovie(db, std::stoi(value));
-    } else {
-      searchMovie(db, value);
+  std::cout << "Enter a command:" << std::endl;
+    while (std::cin >> command) {
+      if (command == "find") {
+        int id;
+        // Calls the findMovie() function to find entries by integer ID.
+        if (!(std::cin >> id)) {
+          break;
+        }
+        findMovie(db, id);
+      } else if (command == "search") {
+          std::string value;
+          // Calls the searchMovie() function to find entries by text.
+          if (!(std::cin >> std::quoted(value))) {
+            break;
+          }
+          searchMovie(db, value);
+      } else if (command == "exit") {
+          // Stop the program if the user wants to exit.
+          break;
+      } else {
+          // Handle wrong commands.
+          std::cout << "Unknown command." << '\n';
+      }
+      // Make sure the program loops until the user wants it to stop.
+      std::cout << "Enter a command:" << '\n';
     }
-  }
   return 0;
 }
